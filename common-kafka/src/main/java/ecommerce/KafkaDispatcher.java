4783ac4c90bd681c0ec68a14f0c4747027d870a1
Rodrigo <rodrigoodcrod1@gmail.com>
2022-10-07 20:19:38 -0300

    Garantindo que todos os brokers tenham a informaçao que fora gravada no leader

--- common-kafka/src/main/java/ecommerce/KafkaDispatcher.java
+++ common-kafka/src/main/java/ecommerce/KafkaDispatcher.java
@@ -14,7 +14,7 @@ public class KafkaDispatcher<T> implements Closeable {
 
     private final KafkaProducer<String, T> producer;
 
-     KafkaDispatcher() {
+     public KafkaDispatcher() {
         this.producer = new KafkaProducer<>(properties());
     }
     private static Properties properties(){
@@ -22,10 +22,11 @@ public class KafkaDispatcher<T> implements Closeable {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
         return properties;
     }
 
-     void send(String topic, String key, T value) throws ExecutionException, InterruptedException {
+    public void send(String topic, String key, T value) throws ExecutionException, InterruptedException {
         var record = new ProducerRecord<>(topic, key, value);
         Callback callback = (data, ex) -> {
             if (ex != null) {
